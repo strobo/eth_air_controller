b7a4054b401ef84a68d1257f60ade7c48e713c6b
strobo <kax551@gmail.com>
2011-10-01 23:13:04 +0900

    update html_data.h

--- html_data.h
+++ html_data.h
@@ -9,12 +9,12 @@ const  prog_char pc_html[] = "\
 <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js'></script>\
 <link rel='stylesheet'href='http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css'>\
 <script>\
-state={a:'off',b:30};state.send=function(){$.get('/ir',{power:state.a,temp:state.b},function(a){console.log(a)})};$(function(){$('#power').text(state.a==='off'?'ON':'OFF').toggle(function(){$(this).text('OFF').toggleClass('primary',!1);state.a='on';state.b=$('#selectTemp').val()-0;state.send()},function(){$(this).text('ON').toggleClass('primary',!0);state.a='off';state.b=$('#selectTemp').val()-0;state.send()});$('#selectTemp').change(function(){state.b=$('#selectTemp').val()-0;state.a==='on'&&state.send()})});\
+state={a:'off',b:30};state.send=function(){$.get('/ir',{power:state.a,temp:state.b},function(a){console.log(a)})};$(function(){$('#power').text(state.a==='off'?'ON':'OFF').toggle(function(){$(this).text('OFF').toggleClass('primary',!1);state.a='on';state.b=$('#temp').val()-0;state.send()},function(){$(this).text('ON').toggleClass('primary',!0);state.a='off';state.b=$('#temp').val()-0;state.send()});$('#temp').change(function(){state.b=$('#temp').val()-0;state.a==='on'&&state.send()})});\
 </script>\
 </head><body>\
 <div class='container'>\
 <h1>RemoteAirController</h1>\
-<p><select id='selectTemp'name='temp'style='font-size:27px;width:100px;height:54px'>\
+<p><select id='temp'name='temp'style='font-size:27px;width:100px;height:54px'>\
 <option value='30'>30</option>\
 <option value='29'>29</option>\
 <option value='28'>28</option>\
